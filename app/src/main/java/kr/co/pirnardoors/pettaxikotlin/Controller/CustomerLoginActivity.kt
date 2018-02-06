37bc3e4e3ad35eed6d4df5df0c55897e88f8a33d
thinkcsww <thinkcsw@gmail.com>
2018-02-06 17:42:22 +0900

    Base

---
+++ app/src/main/java/kr/co/pirnardoors/pettaxikotlin/Controller/CustomerLoginActivity.kt
@@ -0,0 +1,13 @@
+package kr.co.pirnardoors.pettaxikotlin.Controller
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kr.co.pirnardoors.pettaxikotlin.R
+
+class CustomerLoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_customer_login)
+    }
+}
