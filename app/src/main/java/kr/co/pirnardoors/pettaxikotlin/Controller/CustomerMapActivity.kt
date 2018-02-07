c7ffc3658f876ce50ba6b9d856468fe72ab54602
thinkcsww <thinkcsw@gmail.com>
2018-02-07 13:59:10 +0900

    Login project is done!

--- app/src/main/java/kr/co/pirnardoors/pettaxikotlin/Controller/CustomerMapActivity.kt
+++ app/src/main/java/kr/co/pirnardoors/pettaxikotlin/Controller/CustomerMapActivity.kt
@@ -1,8 +1,20 @@
 package kr.co.pirnardoors.pettaxikotlin.Controller
 
+import android.Manifest
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.SyncStateContract.Helpers.update
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.util.Log
+import android.widget.Toast
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -18,6 +30,8 @@ class CustomerMapActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
 
+    var locationManager : LocationManager? = null
+    var locationListener : LocationListener? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_rider_map)
@@ -26,6 +40,11 @@ class CustomerMapActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
                 .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+
+
+
+        //Log out
         logoutBtn.setOnClickListener {
             auth.signOut()
             val intent = Intent(this, MainActivity::class.java)
@@ -38,9 +57,59 @@ class CustomerMapActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        if(locationManager == null) {
+            locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        }
+
+        locationListener = object :LocationListener{
+            override fun onLocationChanged(p0: Location?) {
+                var location = p0
+                updateMap(location)
+            }
+
+            override fun onStatusChanged(p0: String?, p1: Int, p2: Bundle?) {
+
+            }
+
+            override fun onProviderEnabled(p0: String?) {
+            }
+
+            override fun onProviderDisabled(p0: String?) {
+            }
+
+        }
+
+        if(ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            return
+        }
+        locationManager?.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0f, locationListener)
+
+        var lastKnownLocation = locationManager!!.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+        if(lastKnownLocation != null) {
+            var userLocation = LatLng(lastKnownLocation.latitude, lastKnownLocation.longitude)
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation, 15f))
+            mMap.addMarker(MarkerOptions().position(userLocation).title("Your Location"))
+        }
+
     }
+
+    fun updateMap(location : Location?) {
+        if(ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            return
+        }
+        locationManager?.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0f, locationListener)
+
+        var lastKnownLocation = locationManager!!.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+        if(lastKnownLocation != null) {
+            var userLocation = LatLng(lastKnownLocation.latitude, lastKnownLocation.longitude)
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation, 15f))
+            mMap.addMarker(MarkerOptions().position(userLocation).title("Your Location"))
+        }
+
+    }
+
+
+
 }
--- app/src/main/java/kr/co/pirnardoors/pettaxikotlin/Controller/MainActivity.kt
+++ app/src/main/java/kr/co/pirnardoors/pettaxikotlin/Controller/MainActivity.kt
@@ -1,8 +1,13 @@
 package kr.co.pirnardoors.pettaxikotlin.Controller
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.widget.Toast
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_main.*
@@ -14,12 +19,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        if(ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+        && ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this@MainActivity, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION), 100)
+        }
+
         startBtn.setOnClickListener {
             var intent = Intent(this, StartActivity::class.java)
             startActivity(intent)
 
         }
 
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 100) {
+            if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this@MainActivity, "위치정보 권한이 주어졌습니다.", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this@MainActivity, "위치정보 권한이 주어지지 않았습니다. 설정을 변경해주세요.", Toast.LENGTH_LONG).show()
+            }
+        }
     }
 
 }
